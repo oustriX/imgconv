9d8260c62106ce6e553ad672f731b373fdbe380c
oustriX <50222966+oustriX@users.noreply.github.com>
2022-04-20 12:11:57 +1000

    separate encode functions to other file

---
+++ formatsEncode.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/urfave/cli/v2"
+
+func EncodePNG(c *cli.Context) error {
+
+	return nil
+}
+
+func EncodeJPEG(c *cli.Context) error {
+
+	return nil
+}
+
+func EncodeJPG(c *cli.Context) error {
+
+	return nil
+}
