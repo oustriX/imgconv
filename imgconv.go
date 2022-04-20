88872367a93370c8efa84d0246b24e0b71c2fee6
oustriX <50222966+oustriX@users.noreply.github.com>
2022-04-21 08:17:52 +1000

    change flags to commands

--- imgconv.go
+++ imgconv.go
@@ -17,24 +17,21 @@ func main() {
 		CustomAppHelpTemplate: appHelpTemplate,
 		EnableBashCompletion:  true,
 		Version:               "1.0.0",
-		Flags: []cli.Flag{
-			&cli.PathFlag{
-				Name:     "src",
-				Aliases:  []string{"s"},
-				Usage:    "[required] the `ORIGINAL IMAGE` to be converted",
-				Required: true,
+		Commands: []*cli.Command{
+			{
+				Name:    "src",
+				Aliases: []string{"s"},
+				Usage:   "[required] the `ORIGINAL IMAGE` to be converted",
 			},
-			&cli.StringFlag{
-				Name:     "format",
-				Aliases:  []string{"f"},
-				Usage:    "[required] `FORMAT` for the new file",
-				Required: true,
+			{
+				Name:    "newFormat",
+				Aliases: []string{"f"},
+				Usage:   "[required] `FORMAT` for the new file",
 			},
-			&cli.PathFlag{
-				Name:        "destination",
-				Aliases:     []string{"dst", "d"},
-				Usage:       "[required] `PATH` for save image with new format",
-				DefaultText: "./newImage.[format]",
+			{
+				Name:    "destination",
+				Aliases: []string{"dst", "d"},
+				Usage:   "`PATH` for save image with new format (default: ./newImage.[newFormat]",
 			},
 		},
 	}
