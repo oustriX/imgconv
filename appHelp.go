e0ad0d58aab38a1a595c552356020f5d69c4326c
oustriX <50222966+oustriX@users.noreply.github.com>
2022-04-21 08:22:53 +1000

    update help output

--- appHelp.go
+++ appHelp.go
@@ -3,11 +3,15 @@ package main
 const appHelpTemplate = `NAME:
 	{{.Name}} -  {{.Usage}}
 USAGE:
-	imgconv {{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
+	imgconv [src] [newFormat] [dst]
+EXAMPLE:
+	imgconv ./123.jpg png ./1/321.png
+
 SUPPORTED FORMATS:
 	PNG (png)
 	JPEG (jpeg)
 	JPG (jpg)
+	GIF (gif)
 	TIFF (tiff)
 	BMP (bmp)
 	WEBP (webp)
